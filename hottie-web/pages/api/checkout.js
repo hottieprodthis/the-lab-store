54786ffd6c8cc5668cfba8440a83742bcb0ec71c
hottieprodthis <hottieprodthis@gmail.com>
2026-08-27 17:34:33 +0200

    Update checkout.js

--- hottie-web/pages/api/checkout.js
+++ hottie-web/pages/api/checkout.js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
 
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
-      payment_method_types: ['card', 'klarna', 'link'],
+      automatic_payment_methods: { enabled: true },
       allow_promotion_codes: true,
       line_items: [
         {
