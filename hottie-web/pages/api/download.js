7b0c626afd53f104e1e8acabbdeb3ef0e3f67ca4
hottieprodthis <hottieprodthis@gmail.com>
2026-08-29 02:13:03 +0200

    Update download.js

--- hottie-web/pages/api/download.js
+++ hottie-web/pages/api/download.js
@@ -1,6 +1,6 @@
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { r2Client } from '@/lib/r2';
+import { r2Client } from '../../lib/r2';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
