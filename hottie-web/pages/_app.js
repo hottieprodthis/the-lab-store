cc76515c44e547a0bf4ae1eb0bc087b30a6cfe4e
hottieprodthis <hottieprodthis@gmail.com>
2026-08-31 01:26:46 +0200

    Update _app.js

--- hottie-web/pages/_app.js
+++ hottie-web/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Bebas_Neue, Inter } from 'next/font/google';
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
@@ -19,6 +20,11 @@ const body = Inter({
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <Head>
+        <title>The Lab – Hottie</title>
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+      </Head>
       <main className={`${display.variable} ${body.variable} font-body bg-ink min-h-screen bg-grain`}>
         <Component {...pageProps} />
         <CartFloating />
