0b4886c5a9aecf536abfb983f17ab58ad09a45c5
hottieprodthis <hottieprodthis@gmail.com>
2026-08-31 02:02:33 +0200

    Update _app.js

--- hottie-web/pages/_app.js
+++ hottie-web/pages/_app.js
@@ -22,6 +22,40 @@ export default function App({ Component, pageProps }) {
     <CartProvider>
       <Head>
         <title>The Lab – Hottie</title>
+        <link key="favicon-shortcut" rel="shortcut icon" href="/icon.svg?v=3" />
+        <link key="favicon-svg" rel="icon" type="image/svg+xml" href="/icon.svg?v=3" />
+        <link key="apple-icon" rel="apple-touch-icon" href="/icon.svg?v=3" />
+      </Head>
+      <main className={`${display.variable} ${body.variable} font-body bg-ink min-h-screen bg-grain`}>
+        <Component {...pageProps} />
+        <CartFloating />
+      </main>
+    </CartProvider>
+  );
+}import Head from 'next/head';
+import { Bebas_Neue, Inter } from 'next/font/google';
+import '../styles/globals.css';
+import { CartProvider } from '../context/CartContext';
+import CartFloating from '../components/CartFloating';
+
+const display = Bebas_Neue({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-display',
+  display: 'swap',
+});
+
+const body = Inter({
+  subsets: ['latin'],
+  variable: '--font-body',
+  display: 'swap',
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <CartProvider>
+      <Head>
+        <title>The Lab – Hottie</title>
         <link key="favicon" rel="icon" type="image/svg+xml" href="/icon.svg?v=3" />
         <link key="apple-icon" rel="apple-touch-icon" href="/icon.svg?v=3" />
       </Head>
