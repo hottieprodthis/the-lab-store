1db50b15ca6f6a9bb266049a25ae555022e57042
hottieprodthis <hottieprodthis@gmail.com>
2026-09-02 02:12:31 +0200

    Update clases.js

--- hottie-web/pages/clases.js
+++ hottie-web/pages/clases.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import ProductCard from '../components/ProductCard';
 import { supabase } from '../lib/supabaseClient';
 
