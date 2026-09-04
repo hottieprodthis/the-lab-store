49fab41e2e637f3ceb713a0a24e0e1ca92d4e3d7
hottieprodthis <hottieprodthis@gmail.com>
2026-09-05 00:55:52 +0200

    Update _app.js

--- hottie-web/pages/_app.js
+++ hottie-web/pages/_app.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { Bebas_Neue, Inter } from 'next/font/google';
 import { useEffect } from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
 import CartFloating from '../components/CartFloating';
@@ -19,6 +21,8 @@ const body = Inter({
   display: 'swap',
 });
 
+const GA_MEASUREMENT_ID = 'G-BL30X192XS';
+
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     // Desactiva la restauración automática del scroll del navegador
@@ -28,6 +32,15 @@ export default function App({ Component, pageProps }) {
 
     // Fuerza que la página vuelva arriba del todo al cargar o refrescar
     window.scrollTo(0, 0);
+
+    // Si el usuario ya aceptó las cookies anteriormente, concedemos permisos
+    const consent = localStorage.getItem('cookie_consent_accepted');
+    if (consent === 'true' && window.gtag) {
+      window.gtag('consent', 'update', {
+        analytics_storage: 'granted',
+        ad_storage: 'granted',
+      });
+    }
   }, []);
 
   return (
@@ -36,11 +49,31 @@ export default function App({ Component, pageProps }) {
         <title>The Lab – Hottie</title>
         <meta name="google-site-verification" content="Uw-H1BijekkxCzXY17ahyEABFKkhdHYhGaK9ujlPaVo" />
       </Head>
+
+      {/* Bloqueo inicial de cookies según el Consent Mode v2 de Google */}
+      <Script
+        id="google-consent-default"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              'analytics_storage': 'denied',
+              'ad_storage': 'denied'
+            });
+          `,
+        }}
+      />
+
       <main className={`${display.variable} ${body.variable} font-body bg-ink min-h-screen bg-grain`}>
         <Component {...pageProps} />
         <CartFloating />
         <CookieBanner />
       </main>
+
+      {/* Script oficial de Google Analytics */}
+      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
     </CartProvider>
   );
 }
